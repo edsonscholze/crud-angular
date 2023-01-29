ee01217e8a1ddff0183175f480491e029b92a1df
edsonscholze <edson_scholze@hotmail.com>
2023-01-29 16:09:56 -0300

    Inclusão arquivo proxy, para contornar erro de cors.

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { first, tap, delay } from 'rxjs';
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
   constructor(private httpClient: HttpClient) { }
 
   findAll(){
