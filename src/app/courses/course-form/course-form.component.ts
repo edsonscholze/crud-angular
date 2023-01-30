b89452f1bce410c54b28fe36b0ab6fb98a92dbb7
edsonscholze <edson_scholze@hotmail.com>
2023-01-30 20:55:47 -0300

    Integrando com api rest

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,18 +1,47 @@
+import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
 
-  formCourse : FormGroup;
-  constructor(private formBuilder: FormBuilder){
-    this.formCourse = this.formBuilder.group({
-      name:[null],
-      category:[null]
-    });
+  formCourse = this.formBuilder.group({
+    name: [''],
+    category: [''],
+  });
+
+  constructor(
+    private formBuilder: NonNullableFormBuilder,
+    private courseService: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location
+  ) {
+
+  }
+
+  onCancel() {
+    this.location.back();
+  }
+
+  onSubmit() {
+    this.courseService.save(this.formCourse.value).subscribe(
+      (result) => this.onSuccess('Curso salvo com sucesso'),
+      (error) => this.onError('Erro ao salvar curso')
+    );
+  }
+
+  private onError(msg:string) {
+    this.snackBar.open(msg, 'Atenção!', {duration:3000}) ;
+  }
+
+  private onSuccess(msg:string) {
+    this.snackBar.open(msg, 'Sucesso!', {duration:3000}) ;
+    this.onCancel();
   }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -15,8 +15,12 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      //delay(5000),
+      delay(1000),
       tap(courses => console.log(courses))
     )
+  }
+
+  save(record:Partial<Course>){
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 }
